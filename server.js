f58382953716b43d025f88d5cb4d14b9d506d009
elyas <lost_ring123@yahoo.com>
2020-08-16 13:57:35 +0430

    create base of api's but no ui . i need to handle some error

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 
 //socket.io
 const http = require("http");
@@ -26,6 +27,15 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/match", require("./routes/api/match"));
+
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+  //Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 io.on("connect", (socket) => {
   socket.on("join", ({ name, room }, callback) => {
